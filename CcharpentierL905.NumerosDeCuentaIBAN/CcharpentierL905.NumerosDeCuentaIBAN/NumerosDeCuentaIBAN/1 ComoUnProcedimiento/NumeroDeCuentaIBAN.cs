37c79c2a1b2d2d16b3662d4227740912240077b3
Chris <Chris@KAREN-PC>
2016-07-02 09:56:11 -0600

    aaa

--- CcharpentierL905.NumerosDeCuentaIBAN/CcharpentierL905.NumerosDeCuentaIBAN/NumerosDeCuentaIBAN/1 ComoUnProcedimiento/NumeroDeCuentaIBAN.cs
+++ CcharpentierL905.NumerosDeCuentaIBAN/CcharpentierL905.NumerosDeCuentaIBAN/NumerosDeCuentaIBAN/1 ComoUnProcedimiento/NumeroDeCuentaIBAN.cs
@@ -3,7 +3,9 @@
     public class NumeroDeCuentaIBAN
     {
         private static string codigoDePais;
+        private static int elNumeroVerificador;
         private static string elRequerimiento;
+        private static int elRequerimientoComoNumero;
         private static string letrasDelPais;
 
         public static string CalcularNumeroDeCuentaIBAN(string cuentaCliente)
@@ -12,6 +14,13 @@
             codigoDePais = "00";
             elRequerimiento = cuentaCliente + letrasDelPais + codigoDePais;
 
+            elRequerimientoComoNumero = int.Parse(elRequerimiento);
+            elNumeroVerificador = 98 - (elRequerimientoComoNumero % 97);
+
+            if (elNumeroVerificador < 10)
+            {
+
+            }
             return "";
         }
     }
