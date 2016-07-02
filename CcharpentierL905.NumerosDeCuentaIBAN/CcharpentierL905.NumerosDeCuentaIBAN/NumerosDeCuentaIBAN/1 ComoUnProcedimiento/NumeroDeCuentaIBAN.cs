37cd21720ac41f29c48b4f5535c30016723e9338
Chris <Chris@KAREN-PC>
2016-07-02 09:21:52 -0600

    a

---
+++ CcharpentierL905.NumerosDeCuentaIBAN/CcharpentierL905.NumerosDeCuentaIBAN/NumerosDeCuentaIBAN/1 ComoUnProcedimiento/NumeroDeCuentaIBAN.cs
@@ -0,0 +1,18 @@
+﻿namespace ComoUnProcedimiento
+{
+    public class NumeroDeCuentaIBAN
+    {
+        private static string codigoDePais;
+        private static string elRequerimiento;
+        private static string letrasDelPais;
+
+        public static string CalcularNumeroDeCuentaIBAN(string cuentaCliente)
+        {
+            letrasDelPais = "1227";
+            codigoDePais = "00";
+            elRequerimiento = cuentaCliente + letrasDelPais + codigoDePais;
+
+            return "";
+        }
+    }
+}
